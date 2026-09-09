d452d0e4d4c5815f8f3f9efaecbcb24901853c24
maghfirli <Lionelfirli10@gmail.com>
2026-09-09 15:44:52 +0700

    Improve sitemap indexing

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,28 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://portfolio-cyber-lyart.vercel.app";
 
   return [
     {
-      url:
-        "https://portfolio-cyber-lyart.vercel.app",
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
 
-      lastModified:
-        new Date(),
+    {
+      url: `${baseUrl}/portfolio/iso-27001-implementation/documentation`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
 
-      changeFrequency:
-        "monthly",
-
-      priority:
-        1,
+    {
+      url: `${baseUrl}/portfolio/phishing-detection/paper`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
   ];
 }
