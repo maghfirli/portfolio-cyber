3713a614ec5177ba9b236ed23fc0917178961273
maghfirli <Lionelfirli10@gmail.com>
2026-09-09 15:26:20 +0700

    Add sitemap and robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+
+  return [
+    {
+      url:
+        "https://portfolio-cyber-lyart.vercel.app",
+
+      lastModified:
+        new Date(),
+
+      changeFrequency:
+        "monthly",
+
+      priority:
+        1,
+    },
+  ];
+}
